a8d749aef743482f3b28164d5d03630fb7a36c71
Vinicius Neiva <viniciustneiva@gmail.com>
2022-07-30 16:11:51 -0300

    criação do redirect para usuarios logados/não logados

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoFuncionario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller {
+
+    public function redirecionarUsuario() {
+        if (TipoFuncionario::ehGerente() || TipoFuncionario::ehFuncionario()) {
+            return redirect()->intended('/home');
+        } else {
+            if(Auth::check()) {
+                Auth::logout();
+            }
+            return redirect('/');
+        }
+    }
+
+    public function fazerLogin() {
+        if (!Auth::check()) {
+            return view('auth.login');
+        }
+        return $this->redirecionarUsuario();
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'fazerLogin');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
